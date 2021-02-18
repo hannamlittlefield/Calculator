2c13068ec374d798fd5ff8cebdbe68b6c91c6ebf
Hanna Littlefield <hannamlittlefield@gmail.com>
2021-02-18 11:20:36 -0500

    updating app to correct code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <Calculator/>
       </header>
+      <footer className="footer">&copy; 2020 Developed by Hanna Littlefield</footer>
     </div>
   );
 }
